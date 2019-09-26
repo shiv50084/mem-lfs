92907fda555ccbb35dfe9072055ddc1779bd9946
Vladimir Petrigo <taenaru@gmail.com>
2019-09-26 20:10:47 +0300

    mem_thread: Declare attr thread-safe methods

--- mem_thread_adapter.h
+++ mem_thread_adapter.h
@@ -27,4 +27,13 @@ lfs_ssize_t mem_thread_read_f(unsigned fs_id, lfs_t *lfs, lfs_file_t *file,
 lfs_ssize_t mem_thread_write_f(unsigned fs_id, lfs_t *lfs, lfs_file_t *file,
                                const void *buffer, lfs_size_t size);
 
+lfs_ssize_t mem_thread_getattr(unsigned fs_id, lfs_t *lfs, const char *path,
+                               uint8_t type, void *buffer, lfs_size_t size);
+
+int mem_thread_setattr(unsigned fs_id, lfs_t *lfs, const char *path,
+                       uint8_t type, const void *buffer, lfs_size_t size);
+
+int mem_thread_removeattr(unsigned fs_id, lfs_t *lfs, const char *path,
+                          uint8_t type);
+
 #endif // MEM_LFS_MEM_THREAD_ADAPTER_H
