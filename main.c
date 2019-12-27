689d265bbe5e7d6c43bd61b553c9bda5dc7e6df9
Vladimir Petrigo <taenaru@gmail.com>
2019-12-28 01:05:02 +0300

    app: Update with thread-safe methods

--- main.c
+++ main.c
@@ -1,79 +1,25 @@
 #include "lfs.h"
+#include "mem.h"
+#include "mem_thread_adapter.h"
 #include "memory_utils.h"
 
-#include <assert.h>
 #include <stdio.h>
-#include <string.h>
 
 #define MEMORY_STORAGE_SIZE (32U * 1024)
 #define MEMORY_PAGE_SIZE (256UL)
 #define MEMORY_BLOCK_SIZE (2048UL)
 #define MEMORY_BLOCK_COUNT (MEMORY_STORAGE_SIZE / MEMORY_BLOCK_SIZE)
 
-static unsigned char memory_storage[MEMORY_STORAGE_SIZE];
-
 static unsigned char lfs_prog_buffer[MEMORY_PAGE_SIZE];
 static unsigned char lfs_read_buffer[MEMORY_PAGE_SIZE];
 static unsigned char lfs_lookahead_buffer[MEMORY_PAGE_SIZE];
 
-static inline size_t get_address(size_t block_size, lfs_block_t block)
-{
-    size_t base_offset = block_size * block;
-    assert(base_offset < MEMORY_STORAGE_SIZE);
-    return base_offset;
-}
-
-// Read a region in a block. Negative error codes are propogated
-// to the user.
-int memory_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off,
-                void *buffer, lfs_size_t size)
-{
-    lfs_off_t base_offset = get_address(c->block_size, block) + off;
-    printf("%s: base offset: %u\n", __func__, base_offset);
-    memcpy(buffer, memory_storage + base_offset, size);
-
-    return LFS_ERR_OK;
-}
-
-// Program a region in a block. The block must have previously
-// been erased. Negative error codes are propogated to the user.
-// May return LFS_ERR_CORRUPT if the block should be considered bad.
-int memory_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off,
-                const void *buffer, lfs_size_t size)
-{
-    lfs_off_t base_offset = get_address(c->block_size, block) + off;
-    printf("%s: base offset: %u\n", __func__, base_offset);
-    memcpy(memory_storage + base_offset, buffer, size);
-
-    return LFS_ERR_OK;
-}
-
-// Erase a block. A block must be erased before being programmed.
-// The state of an erased block is undefined. Negative error codes
-// are propogated to the user.
-// May return LFS_ERR_CORRUPT if the block should be considered bad.
-int memory_erase(const struct lfs_config *c, lfs_block_t block)
-{
-    lfs_off_t base_offset = get_address(c->block_size, block);
-    printf("%s: base offset: %u\n", __func__, base_offset);
-    //memset(memory_storage + base_offset, 0xff, c->block_size);
-
-    return LFS_ERR_OK;
-}
-
-// Sync the state of the underlying block device. Negative error codes
-// are propogated to the user.
-int memory_sync(const struct lfs_config *c)
-{
-    return LFS_ERR_OK;
-}
-
 static struct lfs_config cfg = {
     // block device operations
-    .read = memory_read,
-    .prog = memory_prog,
-    .erase = memory_erase,
-    .sync = memory_sync,
+    .read = mem_thread_read,
+    .prog = mem_thread_prog,
+    .erase = mem_thread_erase,
+    .sync = mem_thread_sync,
 
     // block device configuration
     .read_size = MEMORY_PAGE_SIZE,
@@ -92,7 +38,8 @@ int main()
     lfs_t fs;
     lfs_file_t file = {0};
 
-    if (memory_utils_restore_dump("memdump", memory_storage, sizeof memory_storage) != MEM_OK)
+    if (memory_utils_restore_dump("memdump", memory_storage_get(),
+                                  MEMORY_STORAGE_SIZE) != MEM_OK)
     {
         printf("%s: Unable to restore memory dump\n", __func__);
     }
@@ -107,7 +54,8 @@ int main()
         }
     }
 
-    if (memory_utils_save_dump("memdump", memory_storage, sizeof memory_storage) != MEM_OK)
+    if (memory_utils_save_dump("memdump", memory_storage_get(),
+                               MEMORY_STORAGE_SIZE) != MEM_OK)
     {
         printf("%s: Unable to save memory dump\n", __func__);
     }
